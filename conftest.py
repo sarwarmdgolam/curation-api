8e35f7cce95006f07cb55c6d3100c13acd789c4d
sarwarmdgolam <mdgolam.sarwar@bracits.com>
2025-05-18 14:04:06 +0600

    Mock redis

---
+++ conftest.py
@@ -0,0 +1,9 @@
+import pytest
+
+@pytest.fixture(autouse=True)
+def use_local_memory_cache(settings):
+    settings.CACHES = {
+        "default": {
+            "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
+        }
+    }
